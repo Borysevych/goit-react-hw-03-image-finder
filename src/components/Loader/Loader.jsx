7cdb8b58bb9d5d46d731a632cfd950f22f5cc8e7
Borysevych <borysevych.rus@gmail.com>
2023-05-14 16:47:19 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,15 @@
-import { Oval } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 import s from '../Loader/Loader.module.css';
 export const Loader = () => {
   return (
     <div className={s.overlay}>
-      <Oval
-        className={s.loader}
-        height={80}
-        width={80}
-        color="#3f51b5"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel="oval-loading"
-        secondaryColor="#3f51b5"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
-      />
+      <RotatingLines
+  strokeColor="grey"
+  strokeWidth="5"
+  animationDuration="0.75"
+  width="96"
+  visible={true}
+/>
     </div>
   );
 };
